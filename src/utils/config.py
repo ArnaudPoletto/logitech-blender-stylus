9c4725ed86be83c22548425e0e0239497e4d2147
ArnaudPoletto <arnaud.poletto@epfl.ch>
2024-06-12 16:16:09 +0200

    clean data folder and added generated folder

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,12 +3,11 @@ import numpy as np
 
 wrk_dir = os.getcwd()
 DATA_PATH = os.path.join(wrk_dir, "..", "data")
-INPUTS_FOLDER = os.path.join(DATA_PATH, "inputs")
 RENDER_FOLDER_PATH = os.path.join(DATA_PATH, "renders")
 
 SEED = None
 if SEED is None:
-    SEED = np.random.randint(0, 2**32 - 1, dtype=np.uint32)
+    SEED = int(np.random.randint(0, 2**32 - 1, dtype=np.uint32))
     print(f"Running with random seed: {SEED}.")
 
 CAMERA_NAME = "Camera"
@@ -23,7 +22,7 @@ FRAME_RATE = 24
 RESOLUTION_DIGITS = 2
 RENDER_RESOLUTION = (640, 480) # (640, 480) # (3840, 2160)
 GROUND_TRUTH_WITH_ARM_MODEL = True # Whether to render the ground truth with the arm model hiding the LEDS, but without reflections on the arm
-HIDE_ARMATURE_PROBABILITY = 0.75 # Probability of hiding the armature during rendering
+HIDE_ARMATURE_PROBABILITY = 0.5 # Probability of hiding the armature during rendering
 ANIMATION_LENGTH = 100
 BACKGROUND_COLOR_SKEW_FACTOR = 1.5
 
